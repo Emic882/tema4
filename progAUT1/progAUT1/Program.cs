cce6d6b261b80bf24162767c6e043969b3ccf858
David Emanuel Muresan <emanuel.muresan@emmanuel.ro>
2023-10-31 00:00:12 +0200

    program autentificare

---
+++ progAUT1/progAUT1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace progAUT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new (string username, string password)[]
+             {
+                 ("vasile", "parola1"),
+                 ("gheorghe", "parola2"),
+                 ("mircea", "parola3")
+             };
+            bool Logged = false;
+
+            do
+            {
+                Console.WriteLine("introdu user ");
+                string inputUsername = Console.ReadLine();
+                Console.WriteLine("introdu parola ");
+                string inputPassword = Console.ReadLine();
+
+                foreach (var user in users)
+                {
+                    if (inputUsername == user.username && inputPassword == user.password)
+                    {
+                        Logged = true;
+                        Console.WriteLine($"Bine ai venit, {inputUsername}!");
+                        break;
+                    }
+                }
+
+                if (!Logged)
+                {
+                    Console.WriteLine("Nume sau parola gresita");
+                }
+            } while (!Logged);
+        }
+    }
+}
